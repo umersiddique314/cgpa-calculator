844ccb8dfe10a6fae060b4b527dc8d160ec94eab
Haseeb Usman <haseebusman030576@gmail.com>
2024-12-17 11:28:42 +0500

    fix: update backend application structure to use application.py and enhance CORS configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
+from config import Config
 from .api import api
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    app.config.from_object(Config)
     
+    # Enable CORS
+    CORS(app, resources={
+        r"/api/*": {
+            "origins": ["https://uaf-cgpa-frontend.azurestaticapps.net"],
+            "supports_credentials": False
+        }
+    })
+    
+    # Register blueprints
     app.register_blueprint(api, url_prefix='/api')
     
     return app
