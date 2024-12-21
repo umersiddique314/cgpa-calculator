97b2e4cf216786acef6be1ddb784aaf24a678134
Haseeb Usman <haseebusman030576@gmail.com>
2024-12-21 17:26:26 +0500

    adjust web.config for gunicorn

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,10 +1,5 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from app import create_app
- 
+
 app = create_app()
 
 if __name__ == "__main__":
