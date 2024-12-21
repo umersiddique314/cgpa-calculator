d20a4938ab10767108d6262335d0637d03b0345f
Haseeb Usman <haseebusman030576@gmail.com>
2024-12-21 16:37:34 +0500

    update

--- backend/application.py
+++ backend/application.py
@@ -1,6 +1,7 @@
 from app import create_app
+from config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
