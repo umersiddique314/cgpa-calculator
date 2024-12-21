37f8fe391573082b8daa491b4891fbd6a982f77a
Haseeb Usman <haseebusman030576@gmail.com>
2024-12-21 16:22:50 +0500

    fix

--- backend/run.py
+++ backend/run.py
@@ -4,9 +4,9 @@ from config import Config
 app = create_app()
 
 if __name__ == "__main__":
-    print("🚀 UAF Result API is starting...")
+    print(" UAF Result API is starting...")
     print(
-        f"📡 API will be available at: http://localhost:{Config.PORT}/api/result?reg_number=XXXX-ag-XXXX"
+        f" API will be available at: http://localhost:{Config.PORT}/api/result?reg_number=XXXX-ag-XXXX"
     )
     app.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
- 
+  
