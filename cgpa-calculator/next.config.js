59799de1236533cb24d9556c3f7c8c2a527a9c62
Haseeb Usman <haseebusman030576@gmail.com>
2025-01-19 17:13:31 +0500

    fix redirect

--- cgpa-calculator/next.config.js
+++ cgpa-calculator/next.config.js
@@ -6,23 +6,6 @@ const nextConfig = {
   },
   swcMinify: true,
   output: 'standalone',
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
-          }
-        ],
-      },
-    ]
-  },
   poweredByHeader: false,
 }
 
