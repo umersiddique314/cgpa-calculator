051319943d60775231cc9e5ecabeb3b11e56260d
Haseeb Usman <haseebusman030576@gmail.com>
2025-01-19 14:30:36 +0500

    feat: add security headers and disable poweredByHeader for improved security

--- cgpa-calculator/next.config.js
+++ cgpa-calculator/next.config.js
@@ -5,7 +5,25 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   swcMinify: true,
-  output: 'standalone'
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          }
+        ],
+      },
+    ]
+  },
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig
